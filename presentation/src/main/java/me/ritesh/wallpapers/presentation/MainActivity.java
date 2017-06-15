b42dc5b728a61e892d43c89a42a6f319c79c69e3
Ritesh <riteshakya037@gmail.com>
2017-06-15 14:11:06 +0545

    first commit

---
+++ presentation/src/main/java/me/ritesh/wallpapers/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package me.ritesh.wallpapers.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.ritesh.wallpapers.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
